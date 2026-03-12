ce98f30b189f0c83cd4492b045606fb7507bcf50
Neeraj Patil <neeraj@Neerajs-MacBook-Pro.local>
2026-03-12 11:51:48 +0530

    completed prosnap backend

--- src/routes/profile.route.js
+++ src/routes/profile.route.js
@@ -3,6 +3,6 @@ const { profile } = require("../controllers");
 const { protectRoute } = require("../middlewares");
 
 router.get("/", protectRoute, profile.getProfileDetails);
-router.get("/:userId", protectRout, profile.getProfileDetails);
+router.get("/:userId", protectRoute, profile.getProfileDetails);
 
 module.exports = router;
